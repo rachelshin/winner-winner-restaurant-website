c3240ff9fbe255b926b5bd039af48764fd0de7e5
Rachel Shin <shin.rachel@gmail.com>
2022-10-18 23:58:28 -0400

    working on mobile

--- js/main.js
+++ js/main.js
@@ -8,9 +8,9 @@ document.querySelector('#smallsClick').addEventListener('click', smallsNext)
 document.querySelector('#bigsClick').addEventListener('click', bigsNext)
 document.querySelector('#drinksClick').addEventListener('click', drinksNext)
 
-document.querySelector('#smallsClick').addEventListener('touchstart', smallsNext)
-document.querySelector('#bigsClick').addEventListener('touchstart', bigsNext)
-document.querySelector('#drinksClick').addEventListener('touchstart', drinksNext)
+// document.querySelector('#smallsClick').addEventListener('touchstart', smallsNext)
+// document.querySelector('#bigsClick').addEventListener('touchstart', bigsNext)
+// document.querySelector('#drinksClick').addEventListener('touchstart', drinksNext)
 
 
 
@@ -18,6 +18,7 @@ function smallsNext(){
     smalls.classList.toggle('hidden')
 	bigs.classList.add('hidden')
 	drinks.classList.add('hidden')
+	//add underline, get rid of underlines
 }
 function drinksNext(){
     smalls.classList.add('hidden')
@@ -31,27 +32,3 @@ function bigsNext(){
 }
 
 
-let spaceship = {
-	homePlanet : 'Earth',
-	color : 'red'
-  };
-//creates changeable spaceship object
-
-  let tryReassignment = obj => {
-	obj = {
-	  identified : false, 
-	  'transport type' : 'flying'
-	}
-	console.log(obj) // Prints {'identified': false, 'transport type': 'flying'}
-	//creates a function where it tries to reassign an object
-   
-  };
-
-  tryReassignment(spaceship) // The attempt at reassignment does not work.
-  spaceship // Still returns {homePlanet : 'Earth', color : 'red'};
-  //parameter is a pointer to the created obj, it is not the spaceship itself
-   
-  spaceship = {
-	identified : false, 
-	'transport type': 'flying'
-  }; // Regular reassignment still works.
